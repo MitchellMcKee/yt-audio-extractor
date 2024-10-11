8c12f8f387e68c9c685bbdf52c7966536e0e5ba7
Mitchell McKee <mckee.m@northeastern.edu>
2024-10-10 23:58:52 -0400

    Added API for returning mp3 file

--- extract.py
+++ extract.py
@@ -1,25 +1,58 @@
-from flask import Flask
+from flask import Flask, request, send_from_directory
 
 app = Flask(__name__)
 
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 
-# Create Youtube Object.
-yt = YouTube('https://www.youtube.com/watch?v=OxAZE1v_BLM&ab_channel=MitchellMcKee', 
-             on_progress_callback=on_progress)
-
-
-audio = yt.streams.filter(only_audio=True).first()
-if audio:
-    print('Downloading Audio...')
+def download_audio(url):
     try:
-        audio.download(filename=f'{yt.title}audio.mp4')
-    except():
-        print()
-else:
-    pass
+        print('fetch: ' + url)
+        yt = YouTube(url, on_progress_callback=on_progress)
+        audio = yt.streams.filter(only_audio=True).first()
+    except:
+        print('Error fetching url')
+    if audio:
+        try:
+            print('Return stream')
+            print(audio)
+            return audio
+        except:
+            print("Failed to download file")
+    else:
+        print('Stream does not exist')
+
+@app.route('/extract')
+def extract_yt_audio():
+    vid = request.args.get('vid')
+    base_url = "https://www.youtube.com/watch?v="
+    fetch_url = base_url + vid
+    try:
+        stream = download_audio(fetch_url)
+    except:
+        print('Error fetching url')
+    if stream:
+        try:
+            filepath = f'./output/{stream.default_filename}'
+            stream.download(filename=filepath)
+            print('Created file')
+        except:
+            print("Failed to download file")
+    else:
+        pass
+    print('Send file')
+    return send_from_directory('output', stream.default_filename)
+
+# ---------------------------------------
+# --------- Use for Debugging -----------
+# ---------------------------------------
+
+# url = input("URL >")
+# download_audio(url)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
